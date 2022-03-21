e7ef62deebd0e33d87a8ff0069ed07cd7e438d75
obrhubr <obrhubr@gmail.com>
2022-03-21 18:13:18 +0100

    :mag: add meta tags

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,24 @@
 import '../styles/globals.css';
 import '../styles/slider.css';
 import Script from 'next/script';
+import Head from 'next/head'
+
+function Meta() {
+	return (
+		<Head>
+			<title>Lycee Francais de Vienne: Donne ton avis sur le menu d&apos;aujourd&apos;hui</title>
+			<meta name="title" content="Lycee Francais de Vienne: Donne ton avis sur le menu d&apos;aujourd&apos;hui"/>
+			<meta name="description" content="Vote tout les jours pour le menu que tu as choisi à la cantine! Le vote est ouvert de 11h40 à 13h50."/>
+  			<meta name="author" content="Niklas Oberhuber"/>
+			<meta name="keywords" content="ranking, voting, cantine, food, survey, lycee, vienne, sondage, lfv"/>
+		</Head>
+	)
+}
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Meta></Meta>
 			<Script
 				src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"
 				strategy="beforeInteractive"
